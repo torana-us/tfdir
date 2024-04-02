88f681c53a5c102fb3dc70f60b978347bbc2e271
Wataru Kurashima <what.r.j150@gmail.com>
2024-04-02 18:03:27 +0900

    style: 💄 apply shellcheck

--- installer.sh
+++ installer.sh
@@ -5,20 +5,20 @@ arch=""
 owner="torana-us"
 repo="tfdir"
 
-if [ $(uname -m) = "x86_64" ]; then
-    arch="amd64"
+if [ "$(uname -m)" = "x86_64" ]; then
+	arch="amd64"
 else
-    arch="arm64"
+	arch="arm64"
 fi
 
 release_id=$(curl -H "Authorization: Bearer $GITHUB_TOKEN" \
-    -sL https://api.github.com/repos/$owner/$repo/releases/latest \
-    | jq '.assets[] | select(.name | contains("'$os'_'$arch'")) | .id')
+	-sL https://api.github.com/repos/$owner/$repo/releases/latest |
+	jq '.assets[] | select(.name | contains("'"$os"'_'"$arch"'")) | .id')
 
 curl -sLJ \
-    -H 'Accept: application/octet-stream' \
-    -H "Authorization: Bearer $GITHUB_TOKEN" \
-    "https://api.github.com/repos/$owner/$repo/releases/assets/$release_id" -o tfdir.tar.gz
+	-H 'Accept: application/octet-stream' \
+	-H "Authorization: Bearer $GITHUB_TOKEN" \
+	"https://api.github.com/repos/$owner/$repo/releases/assets/$release_id" -o tfdir.tar.gz
 
 tar -xvf tfdir.tar.gz
 rm tfdir.tar.gz
